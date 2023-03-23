053d484314728838a468ee55fdb9bf0f69c65aef
Henrique Rubido <henriquerubido1@gmail.com>
2023-03-23 16:52:04 -0300

    first time using java :)

---
+++ src/helloWorld/main/HelloWorld.java
@@ -0,0 +1,8 @@
+package helloWorld.main;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Vem, Meli");
+    }
+}
